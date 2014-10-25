e12404d1a199f1f407ffeb7936158525983a6fef
Joan Lasierra Miró <joan@lasierra.cat>
2014-10-25 14:01:28 +0200

    Codi de prova.

--- mobile/src/main/java/com/finapps/neveralone/AlarmActivity.java
+++ mobile/src/main/java/com/finapps/neveralone/AlarmActivity.java
@@ -1,11 +1,16 @@
 package com.finapps.neveralone;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,10 +21,14 @@ import com.finapps.neveralone.util.Preferences;
 import com.finapps.neveralone.util.UtilBattery;
 import com.finapps.neveralone.util.UtilPictures;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class AlarmActivity extends Activity {
+
+    private static final int NOTIFICATION_ID = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,7 +65,142 @@ public class AlarmActivity extends Activity {
             texto = texto.replace("#" , Float.toString(batery));
             ((TextView)findViewById(R.id.texto)).setText(texto);
         }
+
     }
+
+
+    public static void msgBateria(){
+
+        Context ctx=Application.getContext();
+        NotificationManagerCompat notificationManager;
+        NotificationCompat.Builder notificationBuilder;
+
+        List extraPages = new ArrayList();
+        // creating intent to open main Activity
+
+        // bigstyle notification pages
+        NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
+        // setting title for the notification
+        bigTextStyle.setBigContentTitle("Bateria baja");
+        // setting content
+
+        String texto = ctx.getString(R.string.bateryLow);
+        float batery = UtilBattery.getBatteryCharge(ctx)*100;
+        texto = texto.replace("#" , Float.toString(batery));
+        bigTextStyle.bigText(texto);
+        // creating notification
+        Notification extraPageNotify1 = new NotificationCompat.Builder(Application.getContext())
+                .setStyle(bigTextStyle)
+                        // setting up a background image
+                .setLargeIcon(
+                        BitmapFactory.decodeResource(Application.getContext().getResources(),
+                                R.drawable.ic_launcher)).build();
+        // adding notification to the list
+        extraPages.add(extraPageNotify1);
+
+
+        NotificationCompat.WearableExtender wearExtender = new NotificationCompat.WearableExtender()
+                //adding Collection of pages to the notification
+                .addPages(extraPages)
+                .setHintHideIcon(true)
+                .setBackground(
+                        BitmapFactory.decodeResource(ctx.getResources(),
+                                R.drawable.ic_launcher));
+
+        // Here you create the notification and start adding all the attributes
+        // you are going to use
+        notificationBuilder = new NotificationCompat.Builder(ctx)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle(ctx.getString(R.string.tituloAtencion))
+                .setContentText("Bateria baja")
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setAutoCancel(true)
+                .extend(wearExtender)
+        // adding action that will be launch the main activity again
+        //        .addAction(android.R.drawable.ic_media_play, "GO Back",
+        //                mainPendingIntent)
+        ;
+
+        // Here we instantiate the Notification Manager object to start/stop the
+        // notifications
+        notificationManager = NotificationManagerCompat.from(ctx);
+        notificationManager.notify(NOTIFICATION_ID,
+                notificationBuilder.build());
+    }
+
+
+
+    public static void msg2(){
+
+        Context ctx=Application.getContext();
+        NotificationManagerCompat notificationManager;
+        NotificationCompat.Builder notificationBuilder;
+
+        List extraPages = new ArrayList();
+        // creating intent to open main Activity
+
+            // bigstyle notification pages
+            NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
+            // setting title for the notification
+            bigTextStyle.setBigContentTitle("Big Title");
+            // setting content
+            bigTextStyle.bigText("BigText");
+            // creating notification
+            Notification extraPageNotify1 = new NotificationCompat.Builder(Application.getContext())
+                    .setStyle(bigTextStyle)
+                            // setting up a background image
+                    .setLargeIcon(
+                            BitmapFactory.decodeResource(Application.getContext().getResources(),
+                                    R.drawable.ic_launcher)).build();
+            // adding notification to the list
+            extraPages.add(extraPageNotify1);
+
+        // bigstyle notification pages
+        NotificationCompat.BigTextStyle bigTextStyle2 = new NotificationCompat.BigTextStyle();
+        // setting title for the notification
+        bigTextStyle2.setBigContentTitle("Big Title2");
+        // setting content
+        bigTextStyle2.bigText("BigText2");
+        // creating notification
+        Notification extraPageNotify2 = new NotificationCompat.Builder(Application.getContext())
+                .setStyle(bigTextStyle2)
+                        // setting up a background image
+                .setLargeIcon(
+                        BitmapFactory.decodeResource(Application.getContext().getResources(),
+                                R.drawable.ic_launcher)).build();
+        // adding notification to the list
+        extraPages.add(extraPageNotify2);
+
+
+        NotificationCompat.WearableExtender wearExtender = new NotificationCompat.WearableExtender()
+                //adding Collection of pages to the notification
+                .addPages(extraPages)
+                .setHintHideIcon(true)
+                .setBackground(
+                        BitmapFactory.decodeResource(ctx.getResources(),
+                                R.drawable.ic_launcher));
+
+        // Here you create the notification and start adding all the attributes
+        // you are going to use
+        notificationBuilder = new NotificationCompat.Builder(ctx)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle(ctx.getString(R.string.tituloAtencion))
+                .setContentText(ctx.getString(R.string.atencion_gps2))
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setAutoCancel(true)
+                .extend(wearExtender)
+                        // adding action that will be launch the main activity again
+        //        .addAction(android.R.drawable.ic_media_play, "GO Back",
+        //                mainPendingIntent)
+        ;
+
+        // Here we instantiate the Notification Manager object to start/stop the
+        // notifications
+        notificationManager = NotificationManagerCompat.from(ctx);
+        notificationManager.notify(NOTIFICATION_ID,
+                notificationBuilder.build());
+    }
+
 
     @Override
     public void onBackPressed() {
